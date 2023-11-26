fd8ceaa13a38da091879a3419ac83eec78b03ec1
Olusola.Owoyemi <olusola.owoyemi@cyberspace.net.ng>
2023-11-26 18:04:16 +0100

    fixed Icon

--- src/components/budget/categoryBreakDownItems.tsx
+++ src/components/budget/categoryBreakDownItems.tsx
@@ -16,17 +16,18 @@ const CategoryBreakdownItems:  React.FC<CategoryBreakdownItemsProps> = ({svg, ba
 
     return(
         <Flex marginTop={'20px'} justifyContent={'space-between'} alignItems={'center'}>
-            <Flex gap={'12px'}>
-            <CircularProgress color={progressColor}  fontWeight ={'700'} position={'relative'}  trackColor="white" capIsRound={true} thickness={'4px'}  size={'60px'} value={percentage} >
-                <Center background={background} width={'46px'} height = {'46px'} top= {'0'} left={'0'} bottom={'0'} right={'0'} margin={'auto'} position={'absolute'}  borderRadius={'100%'}>
-                    {svg}
-                </Center>
-            </CircularProgress>
-
-            <Flex paddingBlock={'5px'} flexDirection={'column'} justifyContent={"space-around"}>
-                <Heading fontSize={'14px'}  fontWeight={'500'} textColor={'#001233'}> {categoryName}</Heading>
-                <Text  fontSize={'14px'}  fontWeight={'400'} textColor={'#707480'}>{percentage}%</Text>
-            </Flex>
+            <Flex  gap={'12px'}>
+                <Box height={'61px'}>
+                    <CircularProgress color={progressColor}  fontWeight ={'700'} position={'relative'}  trackColor="white" capIsRound={true} thickness={'4px'}  size={'60px'} value={percentage} >
+                        <Center background={background} width={'46px'} height = {'46px'} top= {'0'} left={'0'} bottom={'0'} right={'0'} margin={'auto'} position={'absolute'}  borderRadius={'100%'}>
+                            {svg}
+                        </Center>
+                    </CircularProgress>
+                </Box>
+                <Flex paddingBlock={'5px'} flexDirection={'column'} justifyContent={"space-around"}>
+                    <Heading fontSize={'14px'}  fontWeight={'500'} textColor={'#001233'}> {categoryName}</Heading>
+                    <Text  fontSize={'14px'}  fontWeight={'400'} textColor={'#707480'}>{percentage}%</Text>
+                </Flex>
             
             </Flex>
 
