9b7bd8ccc2bb3c2445db13103fdf99de8655ef33
Olusola.Owoyemi <olusola.owoyemi@cyberspace.net.ng>
2023-11-26 19:31:14 +0100

    insignificant  update

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import SideBar from "./components/SideBar"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Grid height={'100vh'} templateAreas={{base: `"main" "sideBar"`, sm:  `"main" "sideBar"`, md: `"main" "sideBar"`, lg: `"sideBar main"`}} gridTemplateRows={{base:'1fr 87px', sm: '1fr 87px',md: '1fr 87px', }} gridTemplateColumns={{lg: '87px 1fr '}}>
+    <Grid height={'100vh'} templateAreas={{base: `"main" "sideBar"`, sm:  `"main" "sideBar"`, md: `"main" "sideBar"`, lg: `"sideBar main"`}} gridTemplateRows={{base:'1fr 87px', sm: '1fr 87px',md: '1fr 87px', lg:'none' }} gridTemplateColumns={{lg: '87px 1fr '}}>
       <GridItem display={'flex'} justifyContent={'center'} overflow={'auto'} area={'main'}>
         <Budget/>
       </GridItem>
-      <GridItem area={'sideBar'}>
-        <SideBar />
+      <GridItem  area={'sideBar'}>
+        <SideBar  />
       </GridItem>
     </Grid>
   </ChakraProvider>
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -13,7 +13,7 @@ const SideBar = () => {
 
 
     return (
-        <Flex flexDirection={{base: 'row', sm: 'row', md: 'row', lg: 'column'}} alignItems={{base: 'center', sm: 'center', md: 'center', lg: 'center'}} justifyContent={{base: 'space-around', sm: 'space-around', md:'space-around', lg: 'normal'}} height={{ md: '87px', lg: '100%'}}  width={{ md: '100%', lg: '87px'}}>
+        <Flex flexDirection={{base: 'row', sm: 'row', md: 'row', lg: 'column'}} position={{base:'fixed', sm: 'fixed', md: 'fixed', lg: 'initial' }} bottom={'0'} alignItems={{base: 'center', sm: 'center', md: 'center', lg: 'center'}} justifyContent={{base: 'space-around', sm: 'space-around', md:'space-around', lg: 'normal'}} height={{ md: '87px', lg: '100%'}}  width={{base:'100%', sm: '100%', md: '100%', lg: '87px'}}>
             <SideBarItem svg={<HomeIcon/>} name="Home"/>
             <SideBarItem svg={<ReportsIcon/>} name="Reports"/>
             <SideBarItem svg={<ChatIcon/>} name="Chat"/>
